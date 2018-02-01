72a5c30d5d2c1b4609051c3bec4320c94cf522a8
Spriggs <jack@askewbrook.com>
2018-02-01 22:26:20 +0000

    added base functions to repository abstract as they do not need repeating

--- src/Poplar/Repository.php
+++ src/Poplar/Repository.php
@@ -8,7 +8,7 @@ use Poplar\Exceptions\RepositoryException;
 abstract class Repository implements RepositoryInterface {
 
     /**
-     * @var
+     * @var Model
      */
     protected $model;
 
@@ -36,5 +36,61 @@ abstract class Repository implements RepositoryInterface {
             throw new RepositoryException("Class {$this->model()} must be an instance of \\Poplar\\Model");
         }
         $this->model = $model;
+    }
+
+    public function all($columns = ['*']) {
+        return $this->model->get($columns);
+    }
+
+    public function paginate($perPage = 15, $columns = ['*']) {
+        // TODO: Implement paginate() method.
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return bool|Model
+     * @throws Database\QueryException
+     * @throws \ReflectionException
+     */
+    public function create(array $data) {
+        return $this->model::create($data);
+    }
+
+    /**
+     * @param array $data
+     * @param       $id
+     *
+     * @return Model
+     * @throws Database\QueryException
+     * @throws Exceptions\ModelException
+     * @throws \ReflectionException
+     */
+    public function update(array $data, $id) {
+        return $this->model::update($data,$id);
+    }
+
+    /**
+     * @param $id
+     *
+     * @return int
+     * @throws Database\QueryException
+     */
+    public function delete($id) {
+        return $this->model::destroy($id);
+    }
+
+    /**
+     * @param $id
+     *
+     * @return mixed|static
+     * @throws \ReflectionException
+     */
+    public function find($id) {
+        return $this->model::find($id);
+    }
+
+    public function findBy($field, $value, $columns = ['*']) {
+        return $this->model::where([$field=>$value])->get($columns);
     }
 }
